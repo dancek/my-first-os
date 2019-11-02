f1e2b1f7549be0a1e4e0f7ea982a354a80af5f3a
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-11-02 22:45:53 +0200

    test stuff from tutorial

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,23 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use my1os::println;
+use my1os::{println, serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello {}!", "world");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     my1os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
