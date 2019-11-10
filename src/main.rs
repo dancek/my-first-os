5a98d09e55568bcbf1abc62aa09c8068312563dc
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-11-10 09:27:42 +0200

    stack overflow test

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,6 @@ pub extern "C" fn _start() -> ! {
 
     my1os::init();
 
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
-
-    // trigger a stack overflow
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
